0bba45ffab71ac958bece7e4987b44c73c511bd1
Pengxuan <pengxuan@alumni.nus.edu.sg>
2018-08-27 22:17:05 +0800

    initial trial

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+var dc =require('dc');
+var crossfilter = require('crossfilter2');
+var d3 = require('d3')
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+  class MyChat extends React.Component{
+      constructor(props){
+          super(props);
+      }
+
+    componentDidMount() {
+        fetch('http://localhost:3001/')
+        .then(response => {
+            console.log(response.status);
+            return response.json()})
+        .then(data => {
+            let testchart = dc.barChart('#testchart');
+            var testdata = crossfilter(data.recordsets[0]);
+            var testDataDimension = testdata.dimension((d)=>d.LOT_ID);
+            // var defectDimension = testdata.dimension((d)=>d.TD_Defect_Rate);
+            // defectDimension.filter(d=>d>0.05);
+            let testgroup = testDataDimension.group();
+            testgroup.reduce((p,v)=>p+v.TD_Total_QTY,
+            (p,v)=>p-v.TD_Total_QTY,
+            ()=>0)
+            console.log(testDataDimension.top(10));
+            let groupdata = testgroup.all();
+            let xaxis=[];
+            console.log(groupdata);
+            for(let n =0 ; n<groupdata.length;n++){
+                xaxis.push(groupdata[n].key);
+            }
+            console.log(xaxis);
+            
+
+            testchart
+            .width(500)
+            .height(220)
+            .margins({top: 10, right: 50, bottom: 80, left: 40})
+            .dimension(testDataDimension)
+            .group(testgroup)
+            .elasticY(true) 
+            .x(d3.scaleBand())
+            .xUnits(dc.units.ordinal)
+            .turnOnControls(true)
+            
+            dc.renderAll();
+            
+        })
+    }
+
+    renderAll(){
+        dc.renderAll();
+    }
+
+    render (){
+        return (
+        <div>
+            <p>Total Chart</p>
+            <div id='testchart'></div>
+        </div>
+        )
+    }
+  }
+
+    ReactDOM.render(<MyChat />, document.getElementById('root'));
+
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+const sql = require('mssql')
+
+
+const config = {
+  user: 'mes_fis_admin',
+  password: 'mes$fisadmin',
+  server: 'AMK-ODS-SQL01', // You can use 'localhost\\instance' to connect to named instance
+  database: 'FISREPORT',
+
+  options: {
+      encrypt: true // Use this if you're on Windows Azure
+  }
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  sql.connect(config).then(()=>{
+    
+    return sql.query ` WITH CTE AS
+    (
+       SELECT [AUTO_ID], ERROR_CODE,
+              RN = ROW_NUMBER() OVER (PARTITION BY lens_id 
+                                      ORDER BY [AUTO_ID] DESC)
+       FROM FISREPORT..TEST_SUMMARY_MTBX
+       where last_update > '2018-08-24'
+    ),
+    tst_data as
+    (select
+      tst.STACK_LOT_ID as [Integrated_lot_ID],
+      count(*)  as [TD_Total_QTY],
+      Cast (Count(CASE WHEN ( ERROR_CODE = 91) THEN 1 end) AS Float)/ Cast (count(*)AS Float) as [TD_Defect_Rate],
+      tst.MACHINE_ID as [TD_TesterID]
+    from [dbo].[TEST_SUMMARY_MTBX] tst
+    where  tst.AUTO_ID in (SELECT [AUTO_ID] FROM CTE WHERE RN = 1 )
+    group by tst.STACK_LOT_ID,tst.MACHINE_ID
+    ) 
+    select tst_data.*, ilot.*, substring(tst_data.Integrated_lot_ID,6,1) as [Config] 
+    from firefish2_INTEGRATED_LOT ilot
+    inner join Tst_Data 
+    on tst_data.Integrated_lot_ID = ilot.LOT_ID`;
+  }).then( (results)=>{
+    res.send(results);
+    sql.close();
+  })
+  
+ 
 });
 
 module.exports = router;
